bdea1200449f3701e536c9f658c932c01fa05bd5
(none) <>
2025-04-09 07:53:01 +0000

    rtl8822ce

---
+++ rtl8822ce-install.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+backup_or_restore() {
+    local file="$1"
+    local backup="${file}.back"
+
+    if [ -e "$backup" ]; then
+        cp "$backup" "$file"
+    else
+        cp "$file" "$backup"
+    fi
+}
+
+restore() {
+    local file="$1"
+    local backup="${file}.back"
+    cp "$backup" "$file"
+}
+
+
+RTL=rtl88x2ce # rtl8812au
+DRIVER_TAR="/usr/share/rtl88x2ce-driver/rtl88x2ce-driver.tar.gz"
+CONF_MOD=CONFIG_RTL8822CE
+OLD_DRIVER=rtw88
+parentMakefile="/usr/src/linux/drivers/net/wireless/realtek/Makefile"
+parentKconfig="/usr/src/linux/drivers/net/wireless/realtek/Kconfig"
+driverDir="/usr/src/linux/drivers/net/wireless/realtek/${RTL}"
+# mkdir -p "/lib/modules/$(uname -r)/build"
+# mkdir -p "/lib/modules/$(cat /usr/src/linux/include/config/kernel.release)/build" # gentoo way
+
+# - Add driver to Kernel source tree
+
+rm -r "$driverDir" &> /dev/null
+
+mkdir "$driverDir"
+tar xpf "$DRIVER_TAR" -C "$driverDir"
+
+# - fix line in Makefile of driver
+sed -i "s/export ${CONF_MOD} = m/export ${CONF_MOD} = y/" "$driverDir"/Makefile
+
+# - add line to parent Makefile to our folder
+backup_or_restore "$parentMakefile"
+echo 'obj-$('${CONF_MOD}')		+= '"${RTL}/" >> "$parentMakefile"
+
+# - parent Kconfig - add section to Kconfig with path to our Kconfig
+backup_or_restore "$parentKconfig"
+sed -i '$d' "$parentKconfig" # remove last line
+{
+    echo "source \"drivers/net/wireless/realtek/${RTL}/Kconfig\""
+    echo
+    echo 'endif # WLAN_VENDOR_REALTEK'
+} >> "$parentKconfig"
+
+# - remove rtw88
+backup_or_restore "/usr/src/linux/drivers/net/wireless/realtek/${OLD_DRIVER}/Kconfig"
+backup_or_restore "/usr/src/linux/drivers/net/wireless/realtek/${OLD_DRIVER}/Makefile"
+echo "" > "/usr/src/linux/drivers/net/wireless/realtek/${OLD_DRIVER}/Kconfig"
+echo "" > "/usr/src/linux/drivers/net/wireless/realtek/${OLD_DRIVER}/Makefile"
---
+++ rtl8822ce-remove.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+backup_or_restore() {
+    local file="$1"
+    local backup="${file}.back"
+
+    if [ -e "$backup" ]; then
+        cp "$backup" "$file"
+    else
+        cp "$file" "$backup"
+    fi
+}
+
+restore() {
+    local file="$1"
+    local backup="${file}.back"
+    cp "$backup" "$file"
+}
+
+
+RTL=rtl88x2ce # rtl8812au
+DRIVER_TAR="/usr/share/rtl88x2ce-driver/rtl88x2ce-driver.tar.gz"
+CONF_MOD=CONFIG_RTL8822CE
+OLD_DRIVER=rtw88
+parentMakefile="/usr/src/linux/drivers/net/wireless/realtek/Makefile"
+parentKconfig="/usr/src/linux/drivers/net/wireless/realtek/Kconfig"
+
+rm -rf "/usr/src/linux/drivers/net/wireless/realtek/${RTL}" &>/dev/null
+restore "$parentMakefile"
+restore "$parentKconfig"
+restore "/usr/src/linux/drivers/net/wireless/realtek/${OLD_DRIVER}/Kconfig"
+restore "/usr/src/linux/drivers/net/wireless/realtek/${OLD_DRIVER}/Makefile"
